12fead68a2939772c5697af23f3c7ba3675c3a33
Don Bennett <don.bennett@synthego.com>
2022-10-21 22:22:39 -0700

    Hook to vault/vault-setup.sh if mount is successful.

--- shell/hook.sh
+++ shell/hook.sh
@@ -3,8 +3,15 @@ cd $HOME
 if [ ! -d vault ]; then
     mkdir vault
 fi
+
 if [ ! -f vault/.placeholder ]; then
     echo "Mounting encrypted vault from github"
     sleep 1
     sudo mount -t ecryptfs $HOME/aws-setup/vault $HOME/vault
+fi
+
+if [ -f $HOME/vault/vault-setup.sh ]; then
+    echo "Running ~/vault/vault-setup.sh"
+    sleep 1
+    . $HOME/vault/vault-setup.sh
 fi
