7ebd9967a5f129b7df321765dfa18ac0fda1660e
Don Bennett <don.bennett@synthego.com>
2022-10-23 00:12:39 -0700

    Run the shell hook from /usr/local/aws-setup, should be main branch

--- bin/setup-aws.sh
+++ bin/setup-aws.sh
@@ -38,7 +38,7 @@ function init() {
     chown -R ${USER}:${USER} /home/${USER}/aws-setup
     (cd /home/${USER}/aws-setup && sudo -u ${USER} git checkout -b ${USER} origin/${USER})
 
-    echo '. $HOME/aws-setup/shell/hook.sh' >> /home/${USER}/.bashrc
+    echo '. /usr/local/aws-setup/shell/hook.sh' >> /home/${USER}/.bashrc
 }
 
 ##################################################
