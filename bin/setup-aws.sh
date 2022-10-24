74db3b31605d36d30d96e320f1087bc06b1b2f46
Don Bennett <don.bennett@synthego.com>
2022-10-23 22:38:12 -0700

    cleanup

--- bin/setup-aws.sh
+++ bin/setup-aws.sh
@@ -43,8 +43,6 @@ function add-user() {
 
     sudo rsync -av /usr/local/aws-setup /home/${USER}
     sudo chown -R ${USER}:${USER} /home/${USER}/aws-setup
-    sudo -u ${USER} bash -c "(cd /home/${USER}/aws-setup && git remote rm origin)"
-    sudo -u ${USER} bash -c "(cd /home/${USER}/aws-setup && sudo git remote add origin git@github.com:dpb1857/aws-setup)"
     sudo -u ${USER} bash -c "(cd /home/${USER}/aws-setup && sudo -u ${USER} git checkout -b ${USER} origin/${USER})"
 
     sudo bash -c "echo '. /usr/local/aws-setup/shell/hook.sh' >> /home/${USER}/.bashrc"
