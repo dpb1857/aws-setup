03a84bb7c64acee5823320a8e7d9bb28bc7ee9d2
Don Bennett <don.bennett@synhtego.com>
2022-10-25 06:17:06 +0000

    Add more settings

--- bin-dpb/cinnamon.sh
+++ bin-dpb/cinnamon.sh
@@ -4,3 +4,9 @@
 
 gsettings set org.cinnamon.desktop.session idle-delay 0
 gsettings set org.cinnamon.desktop.screensaver lock-enabled false
+
+gsettings set org.cinnamon.desktop.keybindings.media-keys terminal "['<Primary><Alt>t', '<Primary><Super>s', '<Super>Return']"
+gsettings set org.cinnamon.desktop.keybindings.media-keys www "['XF86WWW', '<Primary><Alt>c']"
+gsettings set org.cinnamon.desktop.keybindings.custom-keybindings/custom0 binding "['<Primary><Alt>x']"
+gsettings set org.cinnamon.desktop.keybindings.custom-keybindings/custom0 command "/bin/bash -ilc /usr/bin/emacs"
+gsettings set org.cinnamon.desktop.keybindings.custom-keybindings/custom0 name "Emacs"
