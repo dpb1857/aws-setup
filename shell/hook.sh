83b395926cae4b7b65bfcad65ee81c01dcebe466
Don Bennett <don.bennett@synthego.com>
2022-10-25 18:48:53 -0700

    vault -> private

--- shell/hook.sh
+++ shell/hook.sh
@@ -1,39 +1,39 @@
 
 export PATH=$HOME/bin:$PATH
 
-if [ -d $HOME/.aws-vault/bin -a ! -a $HOME/bin ]; then
-    ln -s $HOME/.aws-vault/bin $HOME/bin
+if [ -d $HOME/.aws-private/bin -a ! -a $HOME/bin ]; then
+    ln -s $HOME/.aws-private/bin $HOME/bin
 fi
 
 cd $HOME
-if [ ! -d vault ]; then
-    mkdir vault
+if [ ! -d private ]; then
+    mkdir private
 fi
 
-if [ ! -f vault/.placeholder ]; then
+if [ ! -f private/.placeholder ]; then
     echo "***** Mounting encrypted vault from github *****"
     sleep 2
-    chmod 600 $HOME/.aws-vault/vault/dot-ssh/id_rsa
-    sudo mount -t ecryptfs -r $HOME/.aws-vault/vault $HOME/vault
+    chmod 600 $HOME/.aws-private/private/dot-ssh/id_rsa
+    sudo mount -t ecryptfs -r $HOME/.aws-private/private $HOME/private
 
-    if [ ! "`cat vault/.placeholder 2>/dev/null`" = "placeholder" ]; then
-        echo "vault mount failed."
-        sudo umount vault
+    if [ ! "`cat private/.placeholder 2>/dev/null`" = "placeholder" ]; then
+        echo "private mount failed."
+        sudo umount private
         return
     fi
 fi
 
-if [ -f $HOME/vault/vault-setup.sh ]; then
-    echo "Running ~/vault/vault-setup.sh"
-    . $HOME/vault/vault-setup.sh
+if [ -f $HOME/private/private-setup.sh ]; then
+    echo "Running ~/private/private-setup.sh"
+    . $HOME/private/private-setup.sh
 fi
 
-if [ -d $HOME/vault/dot-aws -a ! -a $HOME/.aws ]; then
-    echo "Linking .aws to ~/vault/dot-aws"
-    ln -s $HOME/vault/dot-aws $HOME/.aws
+if [ -d $HOME/private/dot-aws -a ! -a $HOME/.aws ]; then
+    echo "Linking .aws to ~/private/dot-aws"
+    ln -s $HOME/private/dot-aws $HOME/.aws
 fi
 
-if [ -f $HOME/vault/dot-ssh/id_rsa -a ! -f $HOME/.ssh/id_rsa ]; then
+if [ -f $HOME/private/dot-ssh/id_rsa -a ! -f $HOME/.ssh/id_rsa ]; then
     echo "Linking ssh private key to ~/.ssh"
-    ln -s $HOME/vault/dot-ssh/id_rsa $HOME/.ssh/id_rsa
+    ln -s $HOME/private/dot-ssh/id_rsa $HOME/.ssh/id_rsa
 fi
